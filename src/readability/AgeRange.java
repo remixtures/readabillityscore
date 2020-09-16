e60569a341f465cee11fd7fb0ffa5003b200b81b
Miguel Caetano <miguel.a.caetano@gmail.com>
2020-09-16 16:30:09 +0100

    Final average computed

--- src/readability/AgeRange.java
+++ src/readability/AgeRange.java
@@ -2,51 +2,52 @@ package readability;
 
 public class AgeRange {
 
-    public static String getAgeRange(int score) {
-        String ageRange = "";
+    public static int getAgeRange(int score) {
+
+        int ageRange = 0;
 
         switch (score) {
             case 1:
-                ageRange = "6";
+                ageRange = 6;
                 break;
             case 2:
-                ageRange = "7";
+                ageRange = 7;
                 break;
             case 3:
-                ageRange = "9";
+                ageRange = 9;
                 break;
             case 4:
-                ageRange = "10";
+                ageRange = 10;
                 break;
             case 5:
-                ageRange = "11";
+                ageRange = 11;
                 break;
             case 6:
-                ageRange = "12";
+                ageRange = 12;
                 break;
             case 7:
-                ageRange = "13";
+                ageRange = 13;
                 break;
             case 8:
-                ageRange = "14";
+                ageRange = 14;
                 break;
             case 9:
-                ageRange = "15";
+                ageRange = 15;
                 break;
             case 10:
-                ageRange = "16";
+                ageRange = 16;
                 break;
             case 11:
-                ageRange = "17";
+                ageRange = 17;
                 break;
             case 12:
-                ageRange = "18";
+                ageRange = 18;
                 break;
             case 13:
-                ageRange = "24";
+                ageRange = 24;
                 break;
             case 14:
-                ageRange = "24+";
+                ageRange = 25;
                 break;
             default:
                 System.out.println("This text could not be parsed.");
--- src/readability/ReadabilityOptions.java
+++ src/readability/ReadabilityOptions.java
@@ -5,37 +5,47 @@ public class ReadabilityOptions {
     public static void userOptions(String scoreIndexOptions, int numberOfCharacters, int numberOfSyllables,
                                           int numberOfPolysyllables, int numberOfWords, int numberOfSentences) {
 
-        double ari = Indexes.automatedReadabilityIndex(numberOfCharacters, numberOfWords, numberOfSentences);
-        double fk = Indexes.fleschKincaidIndex(numberOfSyllables, numberOfWords, numberOfSentences);
-        double smog = Indexes.smogIndex(numberOfPolysyllables, numberOfSentences);
-        double cl = Indexes.colemanLiauIndex(numberOfCharacters, numberOfWords, numberOfSentences);
+        double ariScore = Indexes.automatedReadabilityIndex(numberOfCharacters, numberOfWords, numberOfSentences);
+        int ariAgeRange = AgeRange.getAgeRange((int) Math.ceil(ariScore));
+
+        double fkScore = Indexes.fleschKincaidIndex(numberOfSyllables, numberOfWords, numberOfSentences);
+        int fkAgeRange = AgeRange.getAgeRange((int) Math.ceil(fkScore));
+
+        double smogScore = Indexes.smogIndex(numberOfPolysyllables, numberOfSentences);
+        int smogAgeRange = AgeRange.getAgeRange((int) Math.ceil(smogScore));
+
+        double clScore = Indexes.colemanLiauIndex(numberOfCharacters, numberOfWords, numberOfSentences);
+        int clAgeRange = AgeRange.getAgeRange((int) Math.ceil(clScore));
+
+        double averageAgeRange = ariAgeRange + fkAgeRange + smogAgeRange + clAgeRange / 4.0;
 
         switch (scoreIndexOptions) {
             case "ARI":
                 System.out.printf("Automated Readability Index: %.2f (about %s year olds).\n",
-                        ari, AgeRange.getAgeRange((int) Math.ceil(ari)));
+                        ariScore, ariAgeRange);
                 break;
             case "FK":
                 System.out.printf("Flesch–Kincaid readability tests: %.2f (about %s year olds).\n",
-                        fk, AgeRange.getAgeRange((int) Math.ceil(fk)));
+                        fkScore, fkAgeRange);
                 break;
             case "SMOG":
                 System.out.printf("Simple Measure of Gobbledygook: %.2f (about %s year olds).\n",
-                        smog, AgeRange.getAgeRange((int) Math.ceil(smog)));
+                        smogScore, smogAgeRange);
                 break;
             case "CL":
                 System.out.printf("Coleman–Liau index: %.2f (about %s year olds).\n",
-                        cl, AgeRange.getAgeRange((int) Math.ceil(cl)));;
+                        clScore, clAgeRange);
                 break;
             case "all":
                 System.out.printf("Automated Readability Index: %.2f (about %s year olds).\n",
-                        ari, AgeRange.getAgeRange((int) Math.ceil(ari)));
+                        ariScore, ariAgeRange);
                 System.out.printf("Flesch–Kincaid readability tests: %.2f (about %s year olds).\n",
-                        fk, AgeRange.getAgeRange((int) Math.ceil(fk)));
+                        fkScore, fkAgeRange);
                 System.out.printf("Simple Measure of Gobbledygook: %.2f (about %s year olds).\n",
-                        smog, AgeRange.getAgeRange((int) Math.ceil(smog)));
+                        smogScore, smogAgeRange);
                 System.out.printf("Coleman–Liau index: %.2f (about %s year olds).\n",
-                        cl, AgeRange.getAgeRange((int) Math.ceil(cl)));;
+                        clScore, clAgeRange);
+                System.out.printf("This text should be understood in average by %.2f year olds.", averageAgeRange);
                 break;
             default:
                 System.out.println("invalid user option");
